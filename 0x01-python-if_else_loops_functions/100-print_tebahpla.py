119996a78dcc7e54ac5ebe7a46f3d56be11c6775
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-03 06:21:08 -0700

    Task 14 - Advanced - re-co

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,11 @@
 # 100-print_tebahpla.py
 
 """Print the ASCII alphabet, in reverse order, alternating lowercase and uppercase"""
-for i in reversed(range(97, 123)):
-    if (i % 2 != 0):
+
+
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2:
         i -= 32
-    print(f"{i:c}", end="")
+    print("{:c}".format(i), end="")
+    if i % 2:
+        i += 32
