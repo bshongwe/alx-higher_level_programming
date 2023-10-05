4ed15a0ae614dd5f00bff6fe176f0b2c404357e2
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-05 03:30:48 -0700

    Task 1 - debug 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# 1-calculation.py
 
 if __name__ == "__main__":
-    """Do some Maths, and print result"""
+    """Do some Maths, and print result."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
