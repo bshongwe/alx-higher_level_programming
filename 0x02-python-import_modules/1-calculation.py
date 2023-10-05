eea0c69c9889b00172925bddf3b732523bab483e
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-05 03:34:14 -0700

    Task 1 - debug 3

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Do some Maths, and print result."""
+    """Print sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
