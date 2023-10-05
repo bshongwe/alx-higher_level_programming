bad8b24281d386a8f53a96479065b5437a18f668
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-04 22:27:33 -0700

    Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 3-infinite_add.py
+
+"""Print result of addition of all arguments"""
+
+
+if __name__ == "__main__":
+    import sys
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
