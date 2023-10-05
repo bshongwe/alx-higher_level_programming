84d83cff910ac3c7faf921b04b6b522633f444f1
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-05 03:22:53 -0700

    Task 0 - debug, re-co

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-# 0-add.py
-
-from add_0 import add
-
-"""Import def add(a, b): from add_0.py and print result"""
-
 
 if __name__ == "__main__":
+    """Print sum of 1 and 2."""
+    from add_0 import add
 
     a = 1
     b = 2
