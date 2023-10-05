fae88bf5d26b2a2b2021460a2417291612c658c0
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-04 22:15:14 -0700

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 2-args.py
+
+"""Print number of and list of its arguments"""
+
+
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
