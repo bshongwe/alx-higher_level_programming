b36e2fef124b8eda3858b77a3e06bac7835863c2
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-05 22:49:30 -0700

    Task 1 - tested

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 1-element_at.py
+
+
+def element_at(my_list, idx):
+    """Retrieve element from list"""
+    length = len(my_list)
+
+    if idx < 0 or idx > length - 1:
+        return (None)
+
+    return(my_list[idx])
