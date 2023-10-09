74493eeb043f27e4caada86d28a82d2b4f11dbdd
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-08 21:53:24 -0700

    Task 1 - correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,9 +4,6 @@
 
 def element_at(my_list, idx):
     """Retrieve element from list"""
-    length = len(my_list)
-
-    if idx < 0 or idx > (length - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
-
     return(my_list[idx])
