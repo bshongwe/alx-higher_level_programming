c41f18eaba31c56c2885848de4bab4405dde16cc
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-08 22:12:03 -0700

    Task 6 - correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     """Print matrix of integers"""
-    for item in matrix:
-        for i in item:
-            print(i, end=" " if i != item[-1] else "")
+    for i_row in matrix:
+        for i_col in i_row:
+            print("{:d}".format(i_col), end=" " if i_col != i_row[-1] else "")
         print()
