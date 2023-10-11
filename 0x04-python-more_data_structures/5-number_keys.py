9b91aab7aac530a9f2334b565522f21345679767
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-11 01:03:57 -0700

    Task 5 & Task 6 - tested

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+
+
+def number_keys(a_dictionary):
+    """Return number of keys in dictionary."""
+    sum = 0
+    keys_list = list(a_dictionary.keys())
+
+    for i in keys_list:
+        sum += 1
+
+        return (sum)
