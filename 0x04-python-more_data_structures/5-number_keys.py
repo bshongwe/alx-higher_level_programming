a95f97a3744701903f382723c2f383032bd70e47
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-11 03:20:23 -0700

    Task 5 - reco

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    sum = 0
-list_keys = list(a_dictionary.keys())
-for i in list_keys:
-    sum += 1
-return (sum)
+    return(len(a_dictionary))
