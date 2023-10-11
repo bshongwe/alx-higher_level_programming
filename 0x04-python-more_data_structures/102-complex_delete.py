4606aff5949da7436456dd6ebd929b2bb0baa7ba
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-11 04:43:37 -0700

    Task 15 - Advanced - experiment tryout

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if a_dictionary:
-        delete = []
-        for k, v in a_dictionary.items():
-            if v == value:
-                delete.append(k)
-        if delete != []:
-            length = len(delete)
-            i = 0
-            while length > 0:
-                a_dictionary.pop(delete[i])
-                i += 1
-                length -= 1
-        return a_dictionary
+    new_dictionary = a_dictionary.copy()
+    delete = []
+    for k, v in a_dictionary.items():
+        if v == value:
+            delete.append(k)
+    if delete != []:
+        for key in delete:
+            del new_dictionary[key]
+    return new_dictionary
