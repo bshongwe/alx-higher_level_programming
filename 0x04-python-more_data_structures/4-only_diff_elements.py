05cec1039debf4c2c186f4330a616f36f0391c48
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-11 03:14:09 -0700

    Task 4 - revert, but with less

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-# 4-only_diff_elements.py
-
-
 def only_diff_elements(set_1, set_2):
-    """Returns a set of all elements present in only one set.
-
-  Args:
-    set_1: A set.
-    set_2: A set.
-
-  Returns:
-    A set of all elements present in only one set.
-  """
-
-  return (set_1 ^ set_2) - (set_1 & set_2)
+    return (set_1 ^ set_2) - (set_1 & set_2)
