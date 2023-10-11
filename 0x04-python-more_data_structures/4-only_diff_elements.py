6b0c78d3cc23a4da6716c28c31b8d3109007615f
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-11 03:11:36 -0700

    Task 4 - debugg, reco

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,5 +3,14 @@
 
 
 def only_diff_elements(set_1, set_2):
-    """Return set of all elements present in only one set."""
-    return(set_1 ^ set_2) - (set_1 & set_2)
+    """Returns a set of all elements present in only one set.
+
+  Args:
+    set_1: A set.
+    set_2: A set.
+
+  Returns:
+    A set of all elements present in only one set.
+  """
+
+  return (set_1 ^ set_2) - (set_1 & set_2)
