bb82efef757b0a77e171fc9e40c92e0bf6527696
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-11 04:15:17 -0700

    Task 15 - Advanced - reco

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    list_keys = list(a_dictionary.keys())
-
-for value_dic in list_keys:
-    if value == a_dictionary.get(value_dic):
-        del a_dictionary[value_dic]
-
-return (a_dictionary)
+    if a_dictionary:
+        delete = []
+        for k, v in a_dictionary.items():
+            if v == value:
+                delete.append(k)
+        if delete != []:
+            length = len(delete)
+            i = 0
+            while length > 0:
+                a_dictionary.pop(delete[i])
+                i += 1
+                length -= 1
+        return a_dictionary
