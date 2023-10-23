074d87b2d25dc8cf2bd5f0a063f6ea51f75d5448
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-23 04:18:23 -0700

    Task 10 - Advanced - edit

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,6 +1,6 @@
 /*
  * File: 103-python.c
- * Auth: Ernest Bhekizwe Shongwe 
+ * Auth: Ernest Bhekizwe Shongwe
  */
 
 #include <Python.h>
@@ -22,17 +22,22 @@ void print_python_list(PyObject *p)
 	const char *type;
 	PyListObject *list = (PyListObject *)p;
 	PyVarObject *var = (PyVarObject *)p;
+
 	size = var->ob_size;
 	alloc = list->allocated;
+
 	fflush(stdout);
+
 	printf("[*] Python list info\n");
 	if (strcmp(p->ob_type->tp_name, "list") != 0)
 	{
-		printf(" [ERROR] Invalid List Object\n");
+		printf("  [ERROR] Invalid List Object\n");
 		return;
 	}
+
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", alloc);
+
 	for (i = 0; i < size; i++)
 	{
 		type = list->ob_item[i]->ob_type->tp_name;
@@ -52,19 +57,25 @@ void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i;
 	PyBytesObject *bytes = (PyBytesObject *)p;
+
 	fflush(stdout);
+
 	printf("[.] bytes object info\n");
 	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
-	{printf(" [ERROR] Invalid Bytes Object\n");
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-	printf(" size: %ld\n", ((PyVarObject *)p)->ob_size);
-	printf(" trying string: %s\n", bytes->ob_sval);
+
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+
 	if (((PyVarObject *)p)->ob_size >= 10)
 		size = 10;
 	else
 		size = ((PyVarObject *)p)->ob_size + 1;
-	printf(" first %ld bytes: ", size);
+
+	printf("  first %ld bytes: ", size);
 	for (i = 0; i < size; i++)
 	{
 		printf("%02hhx", bytes->ob_sval[i]);
@@ -82,16 +93,20 @@ void print_python_bytes(PyObject *p)
 void print_python_float(PyObject *p)
 {
 	char *buffer = NULL;
+
 	PyFloatObject *float_obj = (PyFloatObject *)p;
+
 	fflush(stdout);
+
 	printf("[.] float object info\n");
 	if (strcmp(p->ob_type->tp_name, "float") != 0)
 	{
-		printf(" [ERROR] Invalid Float Object\n");
+		printf("  [ERROR] Invalid Float Object\n");
 		return;
 	}
+
 	buffer = PyOS_double_to_string(float_obj->ob_fval, 'r', 0,
 			Py_DTSF_ADD_DOT_0, NULL);
-	printf(" value: %s\n", buffer);
+	printf("  value: %s\n", buffer);
 	PyMem_Free(buffer);
 }
