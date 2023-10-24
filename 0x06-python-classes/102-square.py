43204f60ed533c37388bddc132411b6221f08b57
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-24 03:44:59 -0700

    Task 9 - Advanced - edit

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
     """ A class that defines a square by its size
     """
+
     def __eq__(self, other):
+        """Define the == comparision to a Square."""
         return self.__size == other.__size
 
     def __lt__(self, other):
+        """Define the < comparison to a Square."""
         return self.__size < other.__size
 
     def __le__(self, other):
+        """Define the <= comparison to a Square."""
         return self.__size <= other.__size
 
     def __ne__(self, other):
+        """Define the != comparison to a Square."""
         return self.__size != other.__size
 
     def __gt__(self, other):
+        "Define the > comparison to a Square."""
         return self.__size > other.__size
 
     def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
         return self.__size >= other.__size
 
     def __init__(self, size=0):
