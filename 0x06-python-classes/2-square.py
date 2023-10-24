88ab14a5732a688aba587fef494ceb3cccd3727c
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-24 03:16:18 -0700

    Task 2 - edit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """ A class that defines a square by its size
     """
+
     def __init__(self, size=0):
         """ Method to initialize the square object
         """
