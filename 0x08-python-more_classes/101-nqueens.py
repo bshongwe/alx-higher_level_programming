6a0d71a66e7caa2892f417a2f2a62522ac6051b4
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-30 04:12:01 -0700

    Task 10 - Advanced - edit

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,67 +1,36 @@
 #!/usr/bin/python3
+"""N queens problem"""
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+try:
+    BOARD_SIZE = int(sys.argv[1])
+except Exception:
+    print("N must be a number")
+    sys.exit(1)
+
+if BOARD_SIZE < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
 
-def print_NQueen(square_y, N):
-    """Create a the format and print the result
-    result = possible variant of the queen position
-    in the chess game"""
-    result = []
-    i = 0
-    for a in square_y:
-        new = []
-        new.append(i)
-        new.append(a)
-        result.append(new)
-        i = i + 1
-    print(result)
-
-
-def check_move(pos_x, square_y):
-    """Check if the square it is available to move or not
-        we check the position between one queen and the
-        posible position of the new queen"""
-    for i in range(pos_x):
-        if square_y[i] == square_y[pos_x]:
-            return False
-        if pos_x - i == abs(square_y[pos_x] - square_y[i]):
-            return False
-    return True
-
-
-def solve_NQueen(N, pos_x, square_y):
-    """Backtracking function:
-    - N: Size of chessboard
-    - pos_x: actual position
-    - square_x: list of solutions to move the queen"""
-    if (pos_x == N):
-        print_NQueen(square_y, N)
+def queens(size, i, a, b, c):
+    """Calculates the positions of the queens"""
+    if i < size:
+        for j in range(size):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(size, i + 1, a + [j],
+                                  b + [i + j], c + [i - j])
     else:
-        for i in range(N):
-            square_y.append(i)
-            if (check_move(pos_x, square_y)):
-                solve_NQueen(N, pos_x + 1, square_y)
-            square_y.pop(-1)
+        yield a
 
-if __name__ == '__main__':
-    import sys
 
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    N = sys.argv[1]
-
-    try:
-        N = int(N)
-    except:
-        print("N must be a number")
-        exit(1)
-
-    if (N < 4):
-        print("N must be at least 4")
-        sys.exit(1)
-
-    # initialize chess game
-    square_y = []
-    pos_x = 0
-    solve_NQueen(N, pos_x, square_y)
+for solution in queens(BOARD_SIZE, 0, [], [], []):
+    solve, i = [], 0
+    for sol in solution:
+        solve.append([i, sol])
+        i += 1
+    print(solve)
