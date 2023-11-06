864475cf9b1e01361477b744405e06cb7b282d99
bshongwe <dru.da.beat.hustle@gmail.com>
2023-11-06 02:35:35 -0800

    Task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
-"""Class definition"""
+"""basegeometryclass"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value for integer and positive"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""class rectangle"""
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectable"""
-
+    """creates rectangle class"""
     def __init__(self, width, height):
-        """Constructor of Initialization of width and height"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        """initializes rectangle"""
+        if not super().integer_validator("width", width):
+            self.__width = width
+        if not super().integer_validator("height", height):
+            self.__height = height
 
     def area(self):
-        """Method: area calculation"""
-        return(self.__width * self.__height)
+        """returns area"""
+        return self.__width * self.__height
 
     def __str__(self):
-        """Builtin str that will executes when print method is used"""
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+        """returns string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
