8f79c3b0ca0947797c119643142e3d81154ee6da
bshongwe <dru.da.beat.hustle@gmail.com>
2023-11-07 03:18:34 -0800

    Task 14 - Advanced

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,43 @@
 #!/usr/bin/python3
-""" Module to print status code """
+"""Module to write a script of dict of Filesize and code_status"""
 import sys
 
 
-class Magic:
-    """ Class to generates instances with dict and size"""
-    def __init__(self):
-        """ Init method """
-        self.dic = {}
-        self.size = 0
+filesize = 0
+count = 0
+stc = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+try:
+    for line in sys.stdin:
+        line = line.split()
 
-    def init_dic(self):
-        """ Initialize dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
+        try:
+            filesize += int(line[-1])
+        except Exception:
+            pass
 
-    def add_status_code(self, status):
-        """ add repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
+        try:
+            code = line[-2]
+            if code in list(stc.keys()):
+                stc[code] += 1
+        except Exception:
+            pass
+        count += 1
 
-    def print_info(self, sig=0, frame=0):
-        """ print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
-                print("{}: {:d}".format(key, self.dic[key]))
+        if count % 10 == 0:
+            print("File size: {:d}".format(filesize))
+            for key in sorted(stc.keys()):
+                if stc[key] != 0:
+                    print("{:s}: {:d}".format(key, stc[key]))
 
+    print("File size: {:d}".format(filesize))
+    for key in sorted(stc.keys()):
+        if stc[key] != 0:
+            print("{}: {:d}".format(key, stc[key]))
 
-if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
-
-    try:
-        for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
-                magic.print_info()
-
-            try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
-                pass
-            nlines += 1
-    except KeyboardInterrupt:
-        magic.print_info()
-        raise
-    magic.print_info()
+except KeyboardInterrupt:
+    print("File size: {:d}".format(filesize))
+    for key in sorted(stc.keys()):
+        if stc[key] != 0:
+            print("{:s}: {:d}".format(key, stc[key]))
+    raise
