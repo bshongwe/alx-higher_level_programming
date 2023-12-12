7d9340171d5550e49e4c9f72b7ce1ff7ac564e90
bshongwe <dru.da.beat.hustle@gmail.com>
2023-12-11 22:05:04 -0800

    Task 8 - line 1 edit

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- Query to display the number of records with id = 89 in a table in MySQL
+-- Query to display the number of records with id = 89 in a table in MySQL server
 SELECT COUNT(*) as id FROM first_table WHERE id=89;
