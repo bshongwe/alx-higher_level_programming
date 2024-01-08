0f1031f04368a328fca0dff4bbeb4ee7e35c5ffc
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-08 04:16:57 -0800

    Task 9 - edit

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-
 function add (a, b) {
-	return a + b;
+	const c = a + b;
+	console.log(c);
 }
 
-const arg2 = parseInt(process.argv[2]);
-const arg3 = parseInt(procss.argv[3]);
-
-const result = add(arg2, arg3);
-console.log(result);
+add(Number(process.argv[2]), Number(procss.argv[3]));
