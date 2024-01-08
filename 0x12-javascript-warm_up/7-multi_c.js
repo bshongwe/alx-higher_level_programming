3fbe6aef4fe1e56acae6b62c840d349783273a1b
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-08 05:07:33 -0800

    Task 7 - reco

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
-if (isNaN(x)) {
-	console.log("Missing number of occurrences");
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
 } else {
-	const arr = new Array(x).fill("C is fun");
-	for (const str of arr) console.log(str);
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
