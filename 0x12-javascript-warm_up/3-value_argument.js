4d2b6b74ec533fd51db985656a502dede64ff6a6
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-08 03:18:32 -0800

    Task 3 - edit

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-const firstArg = process.argv[2];
-
-if (firstArg) {
-	console.log(firstArg);
+if (process.argv[1] === __filename) {
+	if (process.argv[2] === undefined) {
+		console.log("No argument");
+	} else {
+		console.log("Argument found");
+		console.log(process.argv[2]);
+	}
 } else {
-	console.log('No argument');
+	console.log("Arguments found");
 }
