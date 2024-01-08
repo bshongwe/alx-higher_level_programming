bc239ccec5bc5c7b1ac05d686bedcb45a5aca4f7
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-08 03:43:26 -0800

    Task 6 - edit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-const array = ["C is fun", "Python is cool", "Javascript is amazing"];
-
-for (let i = 0; i < array.length; i++) {
-	console.log(array[i]);
+for (const array of ["C is fun", "Python is cool", "Javascript is amazing"]); {
+	console.log(array);
 }
