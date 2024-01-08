e09f813787e8cb2e616552b35a250246a1a51fc7
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-08 05:31:10 -0800

    Task 11 - reco

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-
-const argumentsList = process.argv.slice(2);
-const numbers = argumentsList.map(arg => parseInt(arg));
-
-if (numbers.length < 2) {
-	console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-	const sortedNumbers = numbers.sort((a, b) => b - a);
-	console.log(sortedNumbers[1]);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
