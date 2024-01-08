d73b47e867ba9c145ecea1909a96d4a33c295fdf
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-08 04:45:29 -0800

    Task 9 - reco

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-function const add(a, b) {
-	console.log(a + b);
-};
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
+}
 
-add(2, 3);
+add(Number(process.argv[2]), Number(process.argv[3]));
