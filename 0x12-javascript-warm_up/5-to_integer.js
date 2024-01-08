0eead0af3848f235eddae3352fe021001185bc08
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-08 03:37:53 -0800

    Task 5 - edit

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
-const arg 2 = process.argv[2];
-const parsedNumber = parseInt(arg2);
-
-if (isNan(parsedNumber)) {
-	console.log("Not a number");
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+	console.log('Not a number');
 } else {
-	console.log(`My number: ${parsedNumber}`);
+	console.log('My number:', parseInt(process.argv[2]));
 }
