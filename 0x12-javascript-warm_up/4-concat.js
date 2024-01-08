f661f722690b069ae0db2b91a4d1a0f0f196b0b6
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-08 03:28:48 -0800

    Task 4 - reco

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,12 +1,2 @@
 #!/usr/bin/node
-if (process.argv[1] === __filename) {
-	if (process.argv[2] === undefined) {
-		console.log("No argument");
-	} else if (process.argv[3] === undefined) {
-		console.log("Only one argument found");
-	} else {
-		console.log(process.argv[2] + " is " + process.argv[3]);
-	}
-} else {
-	console.log("Arguments found");
-}
+console.log(process.argv[2] + " is " + process.argv[3]);
