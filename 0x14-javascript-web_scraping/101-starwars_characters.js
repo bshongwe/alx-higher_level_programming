598381034139b07584264143c12a320d9b613cee
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-20 11:02:27 -0800

    Task 8 - Advanced - req >> URL >> JSON for characters >> get req for character >> JSON for characterData --> console.log : debug

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,18 +5,18 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
+request.get(url, (error1, response1, body1) => {
+  if (error1) {
+    console.log(error1);
   } else {
-    const content = JSON.parse(body);
+    const content = JSON.parse(body1);
     const characters = content.characters;
     for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
+      request.get(character, (error2, response2, body2) => {
+        if (error2) {
+          console.log(error2);
         } else {
-          const names = JSON.parse(body);
+          const names = JSON.parse(body2);
           console.log(names.name);
         }
       });
