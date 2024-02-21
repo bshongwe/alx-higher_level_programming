87d6d78ea41ad33bb964ffb7cdb838e3613d7a94
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-21 05:20:07 -0800

    Task 6 - correction: req >> API >> get --> JSON for each --> console.log --> tasks completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,20 +2,29 @@
 // Computes tasksCompleted by userID
 
 const request = require('request');
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-request.get(process.argv[2], { json: true }, (error, response, body) => {
+request.get(apiUrl, { json: true }, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode);
     return;
   }
 
   const tasksCompleted = {};
+
+  // Iterate through each todo item
   body.forEach((todo) => {
+    // If the task is completed, increment the count for that user ID
     if (todo.completed) {
       if (!tasksCompleted[todo.userId]) {
         tasksCompleted[todo.userId] = 1;
       } else {
-        tasksCompleted[todo.userId] += 1;
+        tasksCompleted[todo.userId]++;
       }
     }
   });
